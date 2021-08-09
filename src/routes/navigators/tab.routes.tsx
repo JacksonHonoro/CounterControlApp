684df886403483fb2e1065c536025d15a1e2a9d6
JacksonHonoro <jackson.honoro@gmail.com>
2021-08-09 05:13:49 -0300

    Implementado Icone no TabBar

--- src/routes/navigators/tab.routes.tsx
+++ src/routes/navigators/tab.routes.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MatIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import OctIcon from 'react-native-vector-icons/Octicons';
 
 import { CounterStackRoutes, ConfigStackRoutes } from './stack.routes';
 
@@ -7,8 +9,32 @@ const Tab = createBottomTabNavigator();
 
 const TabRoutes: React.FC = () => (
   <Tab.Navigator screenOptions={{ headerShown: false }}>
-    <Tab.Screen name="Counters" component={CounterStackRoutes} />
-    <Tab.Screen name="Config" component={ConfigStackRoutes} />
+    <Tab.Screen
+      name="TabCounters"
+      component={CounterStackRoutes}
+      options={{
+        title: 'Counters',
+        tabBarIcon: ({ focused }) =>
+          focused ? (
+            <MatIcon name="counter" size={24} color="#0f13e0" />
+          ) : (
+            <MatIcon name="counter" size={22} color="#434343" />
+          ),
+      }}
+    />
+    <Tab.Screen
+      name="TabConfig"
+      component={ConfigStackRoutes}
+      options={{
+        title: 'Config',
+        tabBarIcon: ({ focused }) =>
+          focused ? (
+            <OctIcon name="settings" size={24} color="#0f13e0" />
+          ) : (
+            <OctIcon name="settings" size={22} color="#434343" />
+          ),
+      }}
+    />
   </Tab.Navigator>
 );
 
