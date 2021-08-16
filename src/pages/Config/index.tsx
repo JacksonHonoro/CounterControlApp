14b1e753a63772b03051ec0214e7158e87420763
JacksonHonoro <jackson.honoro@gmail.com>
2021-08-16 05:08:27 -0300

    Finalizado tela Config com redux

--- src/pages/Config/index.tsx
+++ src/pages/Config/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Button from '@/shared/components/Button';
+import Input from '@/shared/components/Input';
 
 import {
   Container,
@@ -9,24 +10,93 @@ import {
   CounterArea,
   Title,
   SelectCounterArea,
+  CounterControlCard,
+  CounterControlContent,
+  Icon,
 } from './styles';
+import {
+  addCounter,
+  removeCounter,
+  removeAllCounters,
+} from '@/shared/store/counterControlStore';
 
-const Config: React.FC = () => (
-  <Container>
-    <CounterArea>
-      <Title>Counters</Title>
-      <ButtonArea>
-        <Button onPress={() => console.log('add')}>Add{'\n'}Counter</Button>
-        <Button onPress={() => console.log('remove')}>
-          Remove{'\n'}Counter
-        </Button>
-      </ButtonArea>
-    </CounterArea>
-    <SelectCounterArea>
-      <Title>Selected Counter</Title>
-      <View />
-    </SelectCounterArea>
-  </Container>
-);
+const Config: React.FC = () => {
+  const [counterValue, setCounterValue] = useState<number>(1);
+  const [counterIndex, setCounterIndex] = useState<number>(1);
+  const dispatch = useDispatch();
+
+  const handleAddCounter = () => {
+    const counterValueFormatted = `0000${counterValue}`.slice(-4);
+    const data = {
+      counterValue: counterValueFormatted,
+      counterIndex,
+    };
+    dispatch(addCounter(data));
+    setCounterIndex(prevState => prevState + 1);
+  };
+
+  const handleRemoveCounter = () => {
+    const counterValueFormatted = `0000${counterValue}`.slice(-4);
+    const data = {
+      counterValue: counterValueFormatted,
+    };
+    dispatch(removeCounter(data));
+  };
+
+  const handleResetCounter = () => {
+    setCounterIndex(1);
+    setCounterValue(1);
+    dispatch(removeAllCounters());
+  };
+
+  return (
+    <Container>
+      <CounterArea>
+        <Title>Counters</Title>
+        <ButtonArea>
+          <Button onPress={() => handleAddCounter()}>Add{'\n'}Counter</Button>
+          <Button onPress={() => handleRemoveCounter()}>
+            Remove{'\n'}Counter
+          </Button>
+          <Button onPress={() => handleResetCounter()}>
+            Reset{'\n'}Counter
+          </Button>
+        </ButtonArea>
+      </CounterArea>
+
+      <SelectCounterArea>
+        <Title>Selected Counter</Title>
+        <CounterControlCard>
+          <CounterControlContent>
+            <Icon
+              name="minus"
+              size={30}
+              onPress={() => {
+                if (counterValue > 1) {
+                  setCounterValue(prevState => Number(prevState) - 1);
+                }
+              }}
+            />
+            <Input
+              onChangeText={value => setCounterValue(value)}
+              keyboardType="numeric"
+              maxLength={4}
+            >
+              {Number(counterValue)}
+            </Input>
+
+            <Icon
+              name="plus"
+              size={30}
+              onPress={() => {
+                setCounterValue(prevState => Number(prevState) + 1);
+              }}
+            />
+          </CounterControlContent>
+        </CounterControlCard>
+      </SelectCounterArea>
+    </Container>
+  );
+};
 
 export default Config;
--- src/pages/Config/styles.ts
+++ src/pages/Config/styles.ts
@@ -1,8 +1,10 @@
 import styled from 'styled-components/native';
+import MatIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export const Container = styled.View`
   flex: 1;
   padding: 12px;
+  background: #2e8dce;
 `;
 
 export const ButtonArea = styled.View`
@@ -17,9 +19,30 @@ export const CounterArea = styled.View`
 
 export const Title = styled.Text`
   font-size: 20px;
-  color: #555;
+  color: #fff;
   font-weight: bold;
   margin-bottom: 6px;
 `;
 
-export const SelectCounterArea = styled.View``;
+export const SelectCounterArea = styled.View`
+  flex: 1;
+`;
+
+export const CounterControlCard = styled.View`
+  flex: 1;
+  border-width: 0.25px;
+  border-radius: 4px;
+  align-items: center;
+  background: #eee;
+`;
+
+export const CounterControlContent = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const Icon = styled(MatIcon)`
+  margin: 0 28px;
+`;
