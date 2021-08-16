fed14f4c9f5cf50efe7737ebc41d0a1b468963f0
JacksonHonoro <jackson.honoro@gmail.com>
2021-08-16 05:14:24 -0300

    Criado componente Input

---
+++ src/shared/components/Input/styles.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex-direction: row;
+  width: 50%;
+  height: 40px;
+  border-radius: 4px;
+  border-width: 0.5px;
+  padding: 0 5px;
+  border-color: #9d9ea3;
+  background: #fff;
+`;
+
+export const TextInput = styled.TextInput`
+  flex: 1;
+`;
